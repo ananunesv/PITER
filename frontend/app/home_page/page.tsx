d20cdd0babf802cbf3ace1ce7328c3521ec36eff
Blazemorales <cunhajp2006@gmail.com>
2025-10-06 11:05:13 -0300

    Adição de imagens (logo unb e logo do piter) e criação do arquivo para a home_page(2).

--- frontend/app/home_page/page.tsx
+++ frontend/app/home_page/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
       <Navbar />
       <main className="container mx-auto px-4 py-8">
         <div className="bg-blue-200">
-          <h1 className="text-2xl font-bold">Bem-vindo à página inicial</h1>
+          <h1 className="text-2xl font-bold">Bem-vindo à página inicial do P.I.T.E.R</h1>
           <p className="text-gray-600 mt-2">Use a barra de pesquisa para começar.</p>
         </div>
       </main>
