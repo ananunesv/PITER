693bd02661f5e5cde99f2450e873543019ccf725
Blazemorales <cunhajp2006@gmail.com>
2025-11-19 10:44:57 -0300

    atualização do front
    Co-authored-by: Pedro Araujo <pedroalucena04@gmail.com>
    
    Co-authored-by: Isabella <isalima2004@gmail.com>
    
    Co-authored-by: GiuliaPaulucci <guilia.paulucci04@gmail.com>
    
    Co-authored-by: Fofodoido <rodrigohenriquedonato@gmail.com>
    
    Co-authored-by: ananunesv <Analu.vieiranunes@gmail.com>
    
    Co-authored-by: Pirata2040 <paulofpnt@gmail.com>
    
    Co-authored-by: Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>.

--- frontend/components/pages/CompareClient.tsx
+++ frontend/components/pages/CompareClient.tsx
@@ -5,6 +5,7 @@ import { SearchForm } from '@/components/molecules/comparing/SearchForm';
 import { useGazetteSearch } from '@/hooks/useGazetteSearch';
 import { GazetteCard } from '@/components/molecules/GazetteCard';
 import Navbar_sec from '@/components/atoms/Navbar_sec';
+import SugestaoPesquisa from '@/components/atoms/SugestaoPesquisa';
 
 export default function CompareClient() {
   const first = useGazetteSearch();
@@ -15,13 +16,13 @@ export default function CompareClient() {
   return (
     <div className="w-full">
       {!showResults ? (
-        <div className="flex flex-col items-center mt-0">
+        <div className="flex flex-col items-center mt-8">
           <div className="w-full mx-auto bg-transparent p-6">
             <Navbar_sec />
           </div>
-          <div className="text-center mb-6">
+          <div className="text-center mb-8">
             <h1 className="text-3xl font-semibold mb-5">Comparar Diários Oficiais</h1>
-            <p className="text-gray-600">Compare investimentos em tecnologia educacional nos diários oficiais do seu município</p>
+            <p className="text-[#01161E]">Compare investimentos em tecnologia educacional nos diários oficiais do seu município</p>
           </div>
           <div className="w-full max-w-4xl">
             <div className="bg-transparent p-6 mb-6">
@@ -39,14 +40,7 @@ export default function CompareClient() {
                   </div>
                 </div>
 
-                <div className="bg-gray-100 rounded-lg p-4 justify-center">
-                  <div className="text-sm font-medium text-gray-700 mb-2">💡 Dicas de Busca</div>
-                  <ul className="text-sm text-gray-700 list-disc pl-5 space-y-1">
-                    <li>Experimente um período de tempo mais amplo</li>
-                    <li>Teste outras categorias tecnológicas</li>
-                    <li>Verifique se há publicações recentes no município</li>
-                  </ul>
-                </div>
+                <SugestaoPesquisa />
               </div>
             </div>
           </div>
